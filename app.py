7d24e18f448d15b4fb4b0e4f5d20b9d1b5258fd5
ChakradharShinde2005 <schakradhar6655@g,mail.com>
2026-07-02 00:01:13 +0530

    Initial CampusFix project

---
+++ app.py
@@ -0,0 +1,293 @@
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+from datetime import datetime
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = "campusfix_secret_key"
+
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role TEXT DEFAULT 'student'
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS complaints (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            title TEXT NOT NULL,
+            department TEXT NOT NULL,
+            description TEXT NOT NULL,
+            image TEXT,
+            status TEXT DEFAULT 'Pending',
+            date TEXT,
+            time TEXT
+        )
+    """)
+
+    cursor.execute("SELECT * FROM users WHERE email='admin@gmail.com'")
+    admin = cursor.fetchone()
+
+    if not admin:
+        cursor.execute("""
+            INSERT INTO users (name, email, password, role)
+            VALUES (?, ?, ?, ?)
+        """, ("Admin", "admin@gmail.com", "admin123", "admin"))
+
+    conn.commit()
+    conn.close()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+                (name, email, password)
+            )
+            conn.commit()
+            conn.close()
+            return redirect("/login")
+
+        except sqlite3.IntegrityError:
+            conn.close()
+            return "Email already registered. Please login."
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE email=? AND password=?",
+            (email, password)
+        )
+
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            session["user_id"] = user[0]
+            session["name"] = user[1]
+            session["email"] = user[2]
+            session["role"] = user[4]
+
+            if session["role"] == "admin":
+                return redirect("/admin_dashboard")
+            else:
+                return redirect("/student_dashboard")
+
+        return "Invalid Email or Password"
+
+    return render_template("login.html")
+
+
+@app.route("/student_dashboard")
+def student_dashboard():
+    if "email" not in session:
+        return redirect("/login")
+
+    if session["role"] != "student":
+        return redirect("/admin_dashboard")
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM complaints WHERE email=?", (session["email"],))
+    total_complaints = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM complaints WHERE email=? AND status='Pending'", (session["email"],))
+    pending_complaints = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM complaints WHERE email=? AND status='Resolved'", (session["email"],))
+    resolved_complaints = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM complaints WHERE email=? AND status='Rejected'", (session["email"],))
+    rejected_complaints = cursor.fetchone()[0]
+
+    conn.close()
+
+    return render_template(
+        "student_dashboard.html",
+        name=session["name"],
+        total_complaints=total_complaints,
+        pending_complaints=pending_complaints,
+        resolved_complaints=resolved_complaints,
+        rejected_complaints=rejected_complaints
+    )
+
+
+@app.route("/add_complaint", methods=["GET", "POST"])
+def add_complaint():
+    if "email" not in session:
+        return redirect("/login")
+
+    if request.method == "POST":
+        title = request.form["title"]
+        department = request.form["department"]
+        description = request.form["description"]
+
+        image = request.files.get("image")
+        image_name = ""
+
+        if image and image.filename != "":
+            image_name = secure_filename(image.filename)
+            image.save(os.path.join(app.config["UPLOAD_FOLDER"], image_name))
+
+        today = datetime.now().strftime("%d-%m-%Y")
+        current_time = datetime.now().strftime("%I:%M %p")
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO complaints
+            (student_name, email, title, department, description, image, date, time)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """, (
+            session["name"],
+            session["email"],
+            title,
+            department,
+            description,
+            image_name,
+            today,
+            current_time
+        ))
+
+        conn.commit()
+        conn.close()
+
+        return redirect("/my_complaints")
+
+    return render_template("add_complaint.html")
+
+
+@app.route("/my_complaints")
+def my_complaints():
+    if "email" not in session:
+        return redirect("/login")
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM complaints WHERE email=? ORDER BY id DESC",
+        (session["email"],)
+    )
+
+    complaints = cursor.fetchall()
+    conn.close()
+
+    return render_template("my_complaints.html", complaints=complaints)
+
+
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    if "email" not in session:
+        return redirect("/login")
+
+    if session["role"] != "admin":
+        return "Access Denied! Admin only."
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM complaints ORDER BY id DESC")
+    complaints = cursor.fetchall()
+
+    conn.close()
+
+    return render_template("admin_dashboard.html", complaints=complaints)
+
+
+@app.route("/update_status/<int:complaint_id>", methods=["POST"])
+def update_status(complaint_id):
+    if "email" not in session:
+        return redirect("/login")
+
+    if session["role"] != "admin":
+        return "Access Denied! Admin only."
+
+    new_status = request.form["status"]
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE complaints SET status=? WHERE id=?",
+        (new_status, complaint_id)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/admin_dashboard")
+
+
+@app.route("/delete_complaint/<int:complaint_id>")
+def delete_complaint(complaint_id):
+    if "email" not in session:
+        return redirect("/login")
+
+    if session["role"] != "admin":
+        return "Access Denied! Admin only."
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM complaints WHERE id=?", (complaint_id,))
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/admin_dashboard")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
